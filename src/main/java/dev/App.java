9d47210dae925f6f1806dc06f24f3a521fae2e93
DUMAS Thierry <afthierry_dumas@yahoo.com>
2019-11-21 17:53:41 +0100

    TP1 Etape 7

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,5 @@
 package dev;
-
+import com.github.lalyos.jfiglet.FigletFont;
 /**
  * Hello world!
  *
@@ -7,7 +7,8 @@ package dev;
 public class App 
 {
     public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    { String asciiArt = FigletFont.convertOneLine("Hello World!");
+    System.out.println(asciiArt);
+       
     }
 }
