400fa939917b37b792385ec9d5d6eadbf5a46741
DUMAS Thierry <afthierry_dumas@yahoo.com>
2019-12-05 20:40:36 +0100

    TP3 : maven : profils

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,14 +1,33 @@
-package dev;
-import com.github.lalyos.jfiglet.FigletFont;
 /**
- * Hello world!
+ * 
+ */
+package dev;
+
+import java.util.ResourceBundle;
+
+import com.github.lalyos.jfiglet.FigletFont;
+
+/**
+ * @author DUMAS
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    { String asciiArt = FigletFont.convertOneLine("Hello World!");
-    System.out.println(asciiArt);
-       
-    }
+public class App {
+	 
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			
+			ResourceBundle bundle = ResourceBundle.getBundle("application");
+			String titre = bundle.getString("titre");
+
+			String asciiArt = FigletFont.convertOneLine(titre);
+			System.out.println(asciiArt);
+			//Affichage de l'environnement
+
+			ResourceBundle env = ResourceBundle.getBundle("application");
+			String environnement = env.getString("environnement");
+			System.out.println("Environnement : " + environnement);
+
+		
+		}
+
 }
